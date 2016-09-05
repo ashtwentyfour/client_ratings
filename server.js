dcc34856e2ec81d8fcdc53c4625e8aed27be95e3
ashtwentyfour <ashwin.menon@accenture.com>
2016-09-04 19:18:51 -0700

    updated server.js to allow retrieval of client assessment info

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var mysql = require('mysql');
+
 var app = express();
 
 var exec = require('child_process').exec
@@ -8,10 +10,14 @@ const EventEmitter = require('events');
 
 var code_compile = 'javac -d rating_system/bin -sourcepath rating_system/src rating_system/src/scoring/*.java';
 
-app.get('/rate_client/:industry/:country', function(req,res){
+/*
+   compute scores for companies from a particular industry and location
+*/
+
+app.post('/rate_client/:industry/:country', function(req,res){
 
     var body = new EventEmitter();
-
+    // compile java classes - scoring system
     var compile = exec(code_compile, function(error, stdout, stderr){
            if (error !== null) {
                console.log('exec error: ' + error);
@@ -19,10 +25,9 @@ app.get('/rate_client/:industry/:country', function(req,res){
            else {
              console.log('code compiled');
              body.emit('update');
-             //res.send('java classes compiled');
            }
     });
-
+    // compute scores and update database
     body.on('update', function() {
       var code_run = 'java -cp rating_system/external_jars/mysql-connector-java-5.1.39-bin.jar:rating_system/bin scoring.Driver';
       code_run = code_run + ' ' + req.params.industry + ' ' + req.params.country;
@@ -35,6 +40,50 @@ app.get('/rate_client/:industry/:country', function(req,res){
     });
 });
 
+/*
+    retrieve all the assessment results for a particular company
+*/
+
+app.get('/getassessmentscores/:client', function(req, res){
+
+  var body = new EventEmitter();
+  // db parameters
+  var con = mysql.createConnection({
+    host: "192.168.99.100",
+    user: "root",
+    password: "abcd",
+    port: 8081,
+    database: 'client_ratings'
+  });
+  // connect to the db and execute queries
+  con.connect(function(err) {
+    if(err) {
+      console.log('Error connecting to Db');
+      return;
+    }
+    console.log('Connection to DB established');
+    body.emit('connected');
+  });
+  // select client info from the client table
+  body.on('connected', function(){
+    var query = 'SELECT * FROM client WHERE client_name = "' + req.params.client + '"';
+    con.query(query,function(err,rows){
+       if(err) throw err;
+       var client_id = (rows[0].client_id).toString();
+       // use the client_id to retrieve the assessment results
+       var query_final = 'SELECT * FROM assessments WHERE client_id = "' + client_id + '"';
+       con.query(query_final, function(err2,rows2){
+          if(err2) throw err2;
+          // return json object with the assessment info
+          res.json(rows2);
+       });
+    });
+  });
+
+});
+
+
+// listen on port 8081
 var server = app.listen(8081, function () {
 
   var host = '0.0.0.0';
